27bfb3499f71f1feec779f9337b6bbd1c4b71d2e
quocanhnt2001 <124774841+quocanhphamnt@users.noreply.github.com>
2024-04-12 11:41:13 +0700

    thêm footer

--- src/app/(page)/layout.tsx
+++ src/app/(page)/layout.tsx
@@ -1,11 +1,13 @@
 import 'tippy.js/dist/tippy.css';
 import Header from '~/common/Header/header';
+import Footer from '~/common/Footer/footer';
 
 export default function ComponentConnectLayout({ children }: { children: React.ReactNode }) {
     return (
         <div>
             <Header></Header>
             {children}
+            <Footer></Footer>
         </div>
     );
 }
---
+++ src/common/Footer/footer.tsx
@@ -0,0 +1,75 @@
+import classNames from 'classnames/bind';
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '../../../public/img/logo.png';
+import styles from './footer.module.scss';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+
+const cx = classNames.bind(styles);
+
+export interface FooterProps {
+}
+
+export default function Footer(props: FooterProps) {
+    return (
+        <Container>
+            <footer className={cx('footer')}>
+                <div className={cx('footer-top')}>
+                    <Image alt="Logo công ty PESTNCLEAN" src={logo.src} width={176} height={100} />
+                    <Link href={'/lienhe'}>
+                        <Button variant="contained" size="large"
+                                sx={{ background: 'var(--primary)' }}
+                        >Liên Hệ</Button>
+                    </Link>
+                </div>
+                <div className={cx('footer-content')}>
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} md={6}>
+                                <div className={cx('footer-content-left')}>
+                                    <p><strong>Địa chỉ: </strong>Lầu 8, 123 Lý Chính Thắng, P. Võ Thị Sau, Q.3</p>
+                                    <p><strong>Điện thoại: </strong>0868 36 36 00</p>
+                                    <p><strong>Email: </strong>sales@petnclea.vn</p>
+                                </div>
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <div className={cx('footer-content-right')}>
+                                    <div>
+                                        <h3 className={cx('title')}>DỊCH VỤ</h3>
+                                        <ul>
+                                            <li><Link href={''}>Kiểm soát côn trùng</Link></li>
+                                            <li><Link href={''}>Dịch vụ vệ sinh</Link></li>
+                                            <li><Link href={''}>Giải pháp vệ sinh</Link></li>
+                                        </ul>
+                                    </div>
+                                    <div>
+                                        <h3 className={cx('title')}>DANH MỤC</h3>
+                                        <ul>
+                                            <li><Link href={''}>Trang chủ</Link></li>
+                                            <li><Link href={''}>Giới thiệu</Link></li>
+                                            <li><Link href={''}>Sản phẩm</Link></li>
+                                            <li><Link href={''}>Blogs</Link></li>
+                                        </ul>
+                                    </div>
+                                    <div>
+                                        <h3 className={cx('title')}>KHÁC</h3>
+                                        <ul>
+                                            <li><Link href={''}>Hỏi đáp</Link></li>
+                                            <li><Link href={''}>Liên hệ</Link></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </div>
+                <div className={cx('footer-bottom')}>
+                    <p>Copyright © 2024 PestNClean. Công ty TNHH Thương Mại Dịch Vụ Xuất Nhập Khẩu Nguyễn Duy.</p>
+                </div>
+            </footer>
+        </Container>
+    );
+}
