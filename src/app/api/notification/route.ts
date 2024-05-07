28a959c7a0340d08ef23b4cad333b8f2f8cd971c
Your Name <you@example.com>
2024-05-07 19:36:13 +0700

    fix api

--- src/app/api/notification/route.ts
+++ src/app/api/notification/route.ts
@@ -1,19 +1,24 @@
-// import { NextResponse } from 'next/server';
-// import { prisma } from '~/libs/orthers/prisma';
+import { NextResponse } from 'next/server';
+import { prisma } from '~/libs/orthers/prisma';
 
-// export async function POST(request: Request) {
-//     const body = await request.json();
-//     const defaultAuthorId = "104399638902286280553";
-//     const newEmail = await prisma.notification.create({
-//         data: {
+export async function POST(request: Request) {
+    const body = await request.json();
+    const defaultAuthorId = '104399638902286280553';
+    const newEmail = await prisma.notification.create({
+        data: {
+            title: body.title,
+            recipientId: body.recipientId,
+            state: body.state,
+            recipient: body.recipient,
+        },
+    });
 
-//         },
-//     });
+    // return NextResponse.json({
+    //     message: "Đăng ký thành công email",
+    //     data: newEmail
+    // }, {
+    //     status: 200,
+    // });
 
-//     // return NextResponse.json({
-//     //     message: "Đăng ký thành công email",
-//     //     data: newEmail
-//     // }, {
-//     //     status: 200,
-//     // });
-// }
+    return NextResponse.json(newEmail);
+}
