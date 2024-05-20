fadf2cc51999cb5e372a53e3cbba3ce4f0919e43
Your Name <you@example.com>
2024-05-20 15:11:28 +0700

    commit

--- src/libs/orthers/axios.ts
+++ src/libs/orthers/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosRequest = axios.create({
-    // baseURL: `https://pestnclean.vercel.app/`,
-    baseURL: `http://localhost:3000/`,
+    baseURL: `https://pestnclean.vercel.app/`,
+    // baseURL: `http://localhost:3000/`,
 });
 
 export const get = async (endpoint: string, body: any) => {
