bdbe5726790863eb5967328a374f08c913dfbd5f
quocanhnt2001 <124774841+quocanhphamnt@users.noreply.github.com>
2024-05-24 10:08:05 +0700

    fix host

--- server.js
+++ server.js
@@ -4,43 +4,42 @@ const next = require('next');
 const { Server } = require('socket.io');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
-const port = 3000;
+const hostname = process.env.NODE_ENV !== 'production' ? 'localhost' : 'pestnclean.vn';
+const port = process.env.PORT || 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 app.prepare().then(() => {
-    const httpServer =createServer(async (req, res) => {
+    const httpServer = createServer(async (req, res) => {
         try {
-          // Be sure to pass `true` as the second argument to `url.parse`.
-          // This tells it to parse the query portion of the URL.
-          const parsedUrl = parse(req.url, true)
-          const { pathname, query } = parsedUrl
-     
-          if (pathname === '/a') {
-                await app.render(req, res, '/a', query)
-          } else if (pathname === '/b') {
-                await app.render(req, res, '/b', query)
-          } else {
-                await handle(req, res, parsedUrl)
-          }
+            // Be sure to pass `true` as the second argument to `url.parse`.
+            // This tells it to parse the query portion of the URL.
+            const parsedUrl = parse(req.url, true);
+            const { pathname, query } = parsedUrl;
+
+            if (pathname === '/a') {
+                await app.render(req, res, '/a', query);
+            } else if (pathname === '/b') {
+                await app.render(req, res, '/b', query);
+            } else {
+                await handle(req, res, parsedUrl);
+            }
         } catch (err) {
-          console.error('Error occurred handling', req.url, err)
-          res.statusCode = 500
-          res.end('internal server error')
+            console.error('Error occurred handling', req.url, err);
+            res.statusCode = 500;
+            res.end('internal server error');
         }
     });
-    
+
     const io = new Server(httpServer);
 
     io.on('connection', (socket) => {
         // console.log(socket.id);
 
-        socket.on("addNotification",(data) => {
-            io.emit("respMessageAddNotify", data);
-        })
+        socket.on('addNotification', (data) => {
+            io.emit('respMessageAddNotify', data);
+        });
     });
-
 
     httpServer
         .once('error', (err) => {
@@ -49,5 +48,5 @@ app.prepare().then(() => {
         })
         .listen(port, () => {
             console.log(`> Ready on http://${hostname}:${port}`);
-    });
+        });
 });
